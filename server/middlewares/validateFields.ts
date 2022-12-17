5226f912ed29b7a670b9698cba420e67971a9d33
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-12-17 00:53:38 -0500

    create validateFields

---
+++ server/middlewares/validateFields.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+import { validationResult } from 'express-validator';
+
+
+const validateFields = ( req:Request , res:Response , next:NextFunction ) =>{
+
+    const errors = validationResult(req);
+    if( !errors.isEmpty() ){
+      return res.status(400).json(errors);
+    }
+
+    next();
+}
+
+
+export default validateFields
